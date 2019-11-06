af702776fce4514757096bc45d2dd79045b144e7
Guilherme Golfetto <guilherme.golfetto@hotmail.com>
2019-11-05 22:12:50 -0300

    final do primeiro curso de web api

---
+++ Alura.WebAPI.AuthProvider/Alura.WebAPI.Api/Controllers/LivrosController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alura.ListaLeitura.Modelos;
+using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Alura.ListaLeitura.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LivrosController : ControllerBase
+    {
+
+        private readonly IRepository<Livro> _repo;
+
+        public LivrosController(IRepository<Livro> repo)
+        {
+            this._repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult ListaDeLivros()
+        {
+            var lista = _repo.All.Select(l => l.ToModel()).ToList();
+            return Ok(lista);
+        }
+
+
+
+        //https://localhost:5001/Livros/{id}
+        [HttpGet("{id}")]
+        public IActionResult Recuperar(int id)
+        {
+            var model = _repo.Find(id);
+
+            if (model == null)
+                return NotFound();
+
+            return Ok(model.ToModel());
+        }
+
+        [HttpGet("{id}/capa")]
+        public IActionResult ImagemCapa(int id)
+        {
+            byte[] img = _repo.All
+                .Where(l => l.Id == id)
+                .Select(l => l.ImagemCapa)
+                .FirstOrDefault();
+            if (img != null)
+            {
+                return File(img, "image/png");
+            }
+            return File("~/images/capas/capa-vazia.png", "image/png");
+        }
+
+        //https://localhost:5001/Livros/ 
+        [HttpPost]
+        public IActionResult Incluir([FromBody]LivroUpload model)
+        {
+            if(ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                _repo.Incluir(livro);
+                var uri = Url.Action("Recuperar", new { id = livro.Id });
+                return Created(uri,livro); // 201
+            }
+
+            return BadRequest(); 
+        }
+
+        //https://localhost:5001/Livros
+        [HttpPut]
+        public IActionResult Alterar([FromBody] LivroUpload model)
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                if (model.Capa == null)
+                {
+                    livro.ImagemCapa = _repo.All
+                        .Where(l => l.Id == livro.Id)
+                        .Select(l => l.ImagemCapa)
+                        .FirstOrDefault();
+                }
+                _repo.Alterar(livro);
+                return Ok(); //200
+            }
+            return BadRequest();
+        }
+
+        //https://localhost:5001/Livros/{id}
+        [HttpDelete("{id}")]
+        public IActionResult Remover(int id)
+        {
+            var model = _repo.Find(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            _repo.Excluir(model);
+            return NoContent(); //204
+        }
+
+    }
+}
---
+++ Alura.WebAPI.AuthProvider/Alura.WebAPI.Api/Formatters/LivroCsvFormatter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Alura.ListaLeitura.Modelos;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers; 
+
+namespace Alura.ListaLeitura.Api.Formatters
+{
+    public class LivroCsvFormatter : TextOutputFormatter
+    {
+
+        public LivroCsvFormatter()
+        {
+            //PRESTA ATENCAO QUE TEM DOIS USING DESSA CLASSE, USAR O DA MICROSOFT, NÃO O SYSTEM
+            var csvMediaType = MediaTypeHeaderValue.Parse("text/csv");
+            var appMediaType = MediaTypeHeaderValue.Parse("application/csv");
+            this.SupportedMediaTypes.Add(csvMediaType);
+            this.SupportedMediaTypes.Add(appMediaType);
+            this.SupportedEncodings.Add(Encoding.UTF8);
+        }
+
+        protected override bool CanWriteType(Type type)
+        {
+            return type == typeof(LivroUpload);
+        }
+
+        public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+        {
+            var livroEmCsv = "";
+
+            if(context.Object is LivroUpload)
+            {
+                var livro = context.Object as LivroUpload;
+
+                livroEmCsv = $"{livro.Titulo};{livro.Subtitulo};{livro.Autor};{livro.Lista}";
+            }
+
+            using (var escritor = context.WriterFactory(context.HttpContext.Response.Body, selectedEncoding))
+            {
+                return escritor.WriteAsync(livroEmCsv);
+            }
+        }
+    }
+}
